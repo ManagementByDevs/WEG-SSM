0ca37a179e13fb2c78140870a1b1b0f3bd8d37c9
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-09-13 14:13:06 -0300

    13/09 - 14:13

--- back-end/src/net/weg/wegssm/model/entities/Pauta.java
+++ back-end/src/net/weg/wegssm/model/entities/Pauta.java
@@ -1,4 +1,37 @@
 package net.weg.wegssm.model.entities;
 
-public class Pauta {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Pauta extends Reuniao {
+
+    private String comissaoDaPauta;
+
+    public void criarAtaDaPauta() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Pauta{" + super.toString() +
+                "comissaoDaPauta='" + comissaoDaPauta + '\'' +
+                '}';
+    }
+
+    public Pauta() {
+        super();
+    }
+
+    public Pauta(String comissaoDaPauta, Date inicioDataReuniao, Date fimDataReuniao, ArrayList<Proposta> propostasDaReuniao) {
+        super(inicioDataReuniao, fimDataReuniao, propostasDaReuniao);
+        this.comissaoDaPauta = comissaoDaPauta;
+    }
+
+    public String getComissaoDaPauta() {
+        return comissaoDaPauta;
+    }
+
+    public void setComissaoDaPauta(String comissaoDaPauta) {
+        this.comissaoDaPauta = comissaoDaPauta;
+    }
 }
