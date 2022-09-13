b83ef4765a857e1ef95fb27e5502df8cb0574469
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-09-13 14:36:41 -0300

    tirando comentários

--- back-end/src/net/weg/wegssm/model/entities/AnalistaDeTI.java
+++ back-end/src/net/weg/wegssm/model/entities/AnalistaDeTI.java
@@ -1,12 +1,6 @@
 package net.weg.wegssm.model.entities;
 
 public class AnalistaDeTI extends Solicitante implements OperacoesGerenciais {
-    //Getters and setters
-    //Construtores (2)
-    //ToString()
-    //Atributos e métodos
-    //Tipo de variável em formato de objeto
-
     public AnalistaDeTI() { }
 
     public AnalistaDeTI(String nomeUsuario, String senhaUsuario, String emailUsuario) {
