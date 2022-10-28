f7d8b509f7e070c540a3a108924d855ef2cb7450
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-28 14:46:20 -0300

    28/10 - 14:45
    Criando o projeto maven

---
+++ back-end-maven/weg-ssm/src/main/java/net/weg/wegssm/WegSsmApplication.java
@@ -0,0 +1,13 @@
+package net.weg.wegssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegSsmApplication.class, args);
+	}
+
+}
---
+++ back-end-maven/weg-ssm/src/test/java/net/weg/wegssm/WegSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.wegssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
