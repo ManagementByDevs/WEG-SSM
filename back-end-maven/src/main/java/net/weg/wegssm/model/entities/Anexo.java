166f5e7a37893ecddb228cde43ab34c6d3103838
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-31 16:30:58 -0300

    31/10 - 16:30
    Entities - Anexo finalizado

--- back-end-maven/src/main/java/net/weg/wegssm/model/entities/Anexo.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/entities/Anexo.java
@@ -1,5 +1,37 @@
 package net.weg.wegssm.model.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "anexo")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
 public class Anexo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private byte[] arquivo;
+
+    // foreign keys
+
+    @ManyToOne
+    @JoinColumn(name = "demanda_id")
+    private Demanda demanda;
+
+    @ManyToOne
+    @JoinColumn(name = "proposta_id")
+    private Proposta proposta;
+
+    @ManyToOne
+    @JoinColumn(name = "escopo_id")
+    private Escopo espoco;
 
 }
--- back-end-maven/src/main/java/net/weg/wegssm/model/entities/Ata.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/entities/Ata.java
@@ -1,4 +1,18 @@
 package net.weg.wegssm.model.entities;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ata")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
 public class Ata {
+
+    private Long id;
+    
 }
