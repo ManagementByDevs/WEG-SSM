13d95b05871e6325090c774517d6ec7b0974eeba
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-31 16:49:30 -0300

    31/10 - 16:49
    DTO - Ata e Demanda finalizados

--- back-end-maven/src/main/java/net/weg/wegssm/dto/AtaDto.java
+++ back-end-maven/src/main/java/net/weg/wegssm/dto/AtaDto.java
@@ -1,4 +1,15 @@
 package net.weg.wegssm.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
 public class AtaDto {
+
+    private Date inicioDataReuniao;
+    private Date fimDataReuniao;
+    private String numeroSequencial;
+
 }
--- back-end-maven/src/main/java/net/weg/wegssm/dto/DemandaDto.java
+++ back-end-maven/src/main/java/net/weg/wegssm/dto/DemandaDto.java
@@ -1,4 +1,22 @@
 package net.weg.wegssm.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import net.weg.wegssm.model.entities.Status;
+
+@Getter @Setter
 public class DemandaDto {
+
+    private String usuarioEmail;
+    private String titulo;
+    private String problema;
+    private String proposta;
+    private String frequencia;
+    private String tamanho;
+    private String buSolicitante;
+    private String busBeneficiadas;
+    private String secaoTI;
+    private String motivoRecusa;
+    private Status status;
+    
 }
