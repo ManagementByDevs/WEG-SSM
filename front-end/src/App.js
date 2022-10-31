3298f17377cfcd7a3631daef50190ff1c27bc3db
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-31 16:47:42 -0300

    31/10 - 16:47
    estrutura do app feita

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,80 @@
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home/home';
+import Login from './pages/Login/login';
+
 import Icon from '@mui/material/Icon';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import { amber, deepOrange, grey } from '@mui/material/colors';
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#00579D'
+//     },
+//   },
+// });
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === 'dark' && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === 'dark' && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === 'light'
+        ? {
+            primary: grey[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: '#fff',
+            secondary: grey[500],
+          }),
+    },
+  },
+});
 
 function App() {
+  const theme = useTheme();
+
   return (
-    <div>
-      Hello world!
-      <Icon>home_outline</Icon>;
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/login" element={<Login></Login>} />
+          <Route path="/" element={<Home></Home>} />
+      </Routes>
+    </Router>
+    // <div>
+    //   Hello world!
+    //   <Icon>home_outline</Icon>;
+    //   <h1 className="text-3xl font-bold underline">
+    //     Hello world!
+    //   </h1>
+    // </div>
   );
 }
 
-export default App;
+const darkModeTheme = createTheme(getDesignTokens('dark'));
+
+export default function DarkThemeWithCustomPalette() {
+  return (
+    <ThemeProvider theme={darkModeTheme}>
+      <App />
+    </ThemeProvider>
+  );
+}
---
+++ front-end/src/pages/Login/login.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function login() {
+    return (
+        <div>
+            <Button>A</Button>
+            login
+        </div>
+    )
+}
+
+export default login
