06e0ac4117fcf1856e90bb1468dd97d4b4a545c6
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-31 15:03:09 -0300

    31/10 - 15:02
    adicionado dependências

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Icon from '@mui/material/Icon';
 
 function App() {
   return (
     <div>
       Hello world!
+      <Icon>home_outline</Icon>;
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
