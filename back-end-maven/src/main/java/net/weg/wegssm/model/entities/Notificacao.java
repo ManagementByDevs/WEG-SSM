4c50907e1a8980902dfd0eccf2595f9bf97d7392
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-10-31 16:44:58 -0300

    16:44 - 31/10
    Feito Notificacao e pauta

--- back-end-maven/src/main/java/net/weg/wegssm/model/entities/Notificacao.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/entities/Notificacao.java
@@ -1,4 +1,37 @@
 package net.weg.wegssm.model.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "notificacao")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Notificacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String notificacao;
+
+    @Column(nullable = false)
+    private Date data;
+
+    @Column(nullable = false)
+    private Long tipoIcone;
+
+    @Column(nullable = false)
+    private String usuarioEmail;
 }
--- back-end-maven/src/main/java/net/weg/wegssm/model/entities/Pauta.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/entities/Pauta.java
@@ -1,4 +1,31 @@
 package net.weg.wegssm.model.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "pauta")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Pauta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date inicioDataReuniao;
+
+    @Column(nullable = false)
+    private Date fimDataReuniao;
+
+    @Column(nullable = false)
+    private String comissao;
 }
