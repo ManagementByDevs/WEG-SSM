a43508687a8d12cb0be8a1bc0e98b958efeb0eaa
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-11-09 19:20:05 -0300

    09/11 - 19:19
    Começando a tabela de proposta

--- back-end-maven/src/main/java/net/weg/wegssm/model/entities/Proposta.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/entities/Proposta.java
@@ -1,4 +1,31 @@
 package net.weg.wegssm.model.entities;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "proposta")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
 public class Proposta {
+
+//    private Long id;
+//    private String titulo;
+//    private String problema;
+//    private String frequencia;
+//    private String tamanho;
+//    private String buSolicitante;
+//    private String secaoTi;
+//    private Status status;
+//    private String motivoRecusa;
+//    private Forum forum;
+//    private Usuario usuario;
+//    private Departamento departamento;
+
 }
