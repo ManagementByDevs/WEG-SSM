42060822b62fe99210c44760a1ad32e4e01ac79a
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-11-17 18:18:11 -0300

    17/11 - 18:18
    arrumado nomes dos arquivos home e login

---
+++ front-end/src/pages/Home/Home.js
@@ -0,0 +1,126 @@
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Paper, Typography, Input, Tab, Box, TextField } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SwapVertIcon from '@mui/icons-material/SwapVert';
+import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+import AddIcon from '@mui/icons-material/Add';
+
+import Header from '../../components/Header/Header';
+import Demanda from '../../components/Demanda/Demanda';
+
+import FontConfig from '../../service/FontConfig';
+import ColorModeContext from '../../service/TemaContext';
+import { grid } from '@mui/system';
+
+const Home = () => {
+  // Desestruturação de objeto em duas variáveis:
+  // - Mode: modo do tema atual ("light" ou "dark")
+  // - toggleColorMode: função para alternar o tema
+  const { mode, toggleColorMode } = useContext(ColorModeContext);
+
+  // UseState para poder visualizar e alterar a aba selecionada
+  const [value, setValue] = React.useState('1');
+
+  // Função para alterar a aba selecionada
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
+  const navigate = useNavigate();
+
+  return (
+    // Container pai
+    <Box sx={{ backgroundColor: 'background.default', height: '100vh', width: '100vw' }}>
+      <Header />
+      <Button variant="contained" onClick={toggleColorMode}>Contained</Button>
+      {/* <Button color='secondary' variant="contained" onClick={toggleColorMode} sx={{fontSize: FontConfig.medium}}>Contained</Button> */}
+      {/* home
+      <Paper sx={{ backgroundColor: 'input.main' }}>
+        <Input sx={{ backgroundColor: 'input.main' }} />
+        TEset
+      </Paper>
+      <Typography fontSize={FontConfig.title} variant="h2" color='text.primary'>Teste com texto</Typography> */}
+
+      {/* Div container */}
+      <Box className='flex justify-center mt-12' sx={{ backgroundColor: 'background.default', width: '100%' }}>
+
+        {/* Div container para o conteúdo da home */}
+        <Box sx={{ width: '90%' }}>
+
+          {/* Sistema de abas */}
+          <TabContext value={value}>
+            <Box className='mb-4' sx={{ borderBottom: 1, borderColor: 'divider.main' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab sx={{ color: 'text.secondary', fontSize: FontConfig.medium }} label="Meu Departamento" value="1" />
+                <Tab sx={{ color: 'text.secondary', fontSize: FontConfig.medium }} label="Minhas Demandas" value="2" />
+              </TabList>
+            </Box>
+
+            {/* Container das ações abaixo das abas (input de pesquisa, filtrar e criar demanda) */}
+            <Box className='flex justify-between w-full'>
+
+              {/* Container para o input e botão de filtrar */}
+              <Box className='flex gap-4 w-2/4'>
+
+                {/* Input de pesquisa */}
+                <Box className='flex justify-between border px-3 py-1' sx={{ backgroundColor: 'input.main', width: "50%" }}>
+
+                  {/* Input de pesquisa */}
+                  <Box className='w-full' component="input" sx={{ backgroundColor: 'input.main', outline: 'none', color: "text.primary", fontSize: FontConfig.medium }} placeholder="Pesquisar por título..." />
+
+                  {/* Container para os ícones */}
+                  <Box className='flex gap-2'>
+
+                    {/* Ícone de pesquisa */}
+                    <SearchOutlinedIcon sx={{ color: 'text.secondary' }} />
+
+                    {/* Ícone de ordenação */}
+                    <SwapVertIcon className='cursor-pointer' sx={{ color: 'text.secondary' }} />
+                  </Box>
+                </Box>
+
+                {/* Botão de filtrar */}
+                <Button sx={{ backgroundColor: 'primary.main', color: 'text.white', fontSize: FontConfig.default }} variant="contained" disableElevation>Filtrar <FilterAltOutlinedIcon /></Button>
+              </Box>
+
+              {/* Botão de criar demanda */}
+              <Button className='gap-2' sx={{ backgroundColor: 'primary.main', color: 'text.white', fontSize: FontConfig.default }}
+                variant="contained" disableElevation
+                onClick={() => { navigate("/criar-demanda") }}>
+                Criar demanda
+                <AddIcon />
+              </Button>
+            </Box>
+
+            {/* Container para o conteúdo das abas */}
+            <Box className='mt-6'>
+
+              {/* Valores para as abas selecionadas */}
+              <TabPanel sx={{ padding: 0 }} value="1">
+                <Box sx={{ display: 'grid', gap: '1rem', gridTemplateColumns: 'repeat(auto-fit, minmax(650px, 1fr))' }}>
+                  <Demanda demanda={{ status: "Aguardando revisão", dono: "Kenzo", tela: "meuDepartamento"}} />
+                  <Demanda demanda={{ status: "Aguardando revisão", dono: "Felipe", tela: "meuDepartamento"}} />
+                  <Demanda demanda={{ status: "Aguardando revisão", dono: "Matheus", tela: "meuDepartamento"}} />
+                  <Demanda demanda={{ status: "Aguardando revisão", dono: "Thiago", tela: "meuDepartamento"}} />
+                </Box>
+              </TabPanel>
+              <TabPanel sx={{ padding: 0 }} value="2">
+                <Box sx={{ display: 'grid', gap: '1rem', gridTemplateColumns: 'repeat(auto-fit, minmax(650px, 1fr))' }}>
+                  <Demanda demanda={{ status: "Aguardando edição", dono: "Thiago", tela: "minhasDemandas"}} />
+                  <Demanda demanda={{ status: "Aguardando revisão", dono: "Thiago", tela: "minhasDemandas"}} />
+                  <Demanda demanda={{ status: "Aprovada", dono: "Thiago", tela: "minhasDemandas"}} />
+                  <Demanda demanda={{ status: "Reprovada", dono: "Thiago", tela: "minhasDemandas"}} />
+                </Box>
+              </TabPanel>
+            </Box>
+          </TabContext>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Home;
---
+++ front-end/src/pages/Login/Login.js
@@ -0,0 +1,84 @@
+import { Button, Paper, TextField, InputAdornment, FormControlLabel, Checkbox, Typography, IconButton } from '@mui/material'
+import { React, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+
+import Header from '../../components/Header/Header';
+
+import LogoWeg from "../../assets/logo-weg.png";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import FontConfig from '../../service/FontConfig';
+
+const Login = (props) => {
+    // Variável para usar função de navegação do react-router-dom
+    let navigate = useNavigate();
+
+    // Variável usada para a visibilidade da senha (true - Senha Invisível / false - Senha Visível)
+    const [senha, setSenha] = useState(true);
+
+    // Função para mudar a visualização da senha (ver ou não)
+    const mudarVisualizacaoSenha = () => {
+        setSenha(!senha);
+    }
+
+    return (
+        // Div Principal com width preenchendo a tela
+        <Paper square sx={{ height: '100vh', width: '100vw' }}>
+            {/* Componente Header */}
+            <Header />
+
+            {/* Div principal abaixo do Header */}
+            <Paper sx={{ height: '91%' }} className='flex justify-center items-center'>
+                {/* Div Principal com as opções do login */}
+                <Paper sx={{ backgroundColor: 'background.default', width: "28%", height: "63%" }} className=' '>
+                    <div className='w-full h-full border-t-12 border-[#00579D] rounded shadow-2xl flex flex-col items-center justify-center space-y-10'>
+                        {/* Logo WEG */}
+                        <img className='w-3/12' src={LogoWeg}></img>
+                        {/* Input de texto do email do usuário */}
+                        <TextField className='w-8/12' id="filled-basic" label="Email" variant="filled" color='primary' />
+                        {/* Input de senha (usa os ícones e o tipo do input conforme a variável "senha", também chamando a função
+                        "mudarVisualizacaoSenha" no click) */}
+                        <TextField
+                            className='w-8/12'
+                            id="input-with-icon-textfield"
+                            label="Senha"
+                            color='primary'
+                            type={senha ? 'password' : 'text'}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="Mudar visibilidade senha"
+                                            onClick={mudarVisualizacaoSenha}
+                                            edge="end"
+                                        >
+                                            {senha ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                            variant="filled"
+                        />
+                        {/* Div para checkbox "lembrar-me" e "Esqueci a senha" */}
+                        <div className='w-8/12 flex justify-between items-center'>
+                            {/* Checkbox com label para função de "lembrar-me" */}
+                            <FormControlLabel control={<Checkbox />} label="Lembrar-me" />
+                            {/* Texto "Esqueci a Senha" (Usa a fonte média) */}
+                            <Typography fontSize={FontConfig.medium} variant="h2" color='text.primary' className='underline hover:cursor-pointer'>Esqueci a Senha</Typography>
+                        </div>
+                        {/* Div para centralizar o botão de login */}
+                        <div className='w-8/12 flex justify-center'>
+
+                            {/* Botão para entrar no sistema */}
+                            <Button onClick={() => { navigate("/") }} variant="contained" size="large" color='primary' className='self-end w-2/6'>
+                                Entrar
+                            </Button>
+                        </div>
+                    </div>
+                </Paper>
+            </Paper>
+        </Paper>
+    )
+}
+
+export default Login
