cd6b17b0f0d70aa77694014030bed64fed4b303d
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-18 13:07:09 -0300

    13:06 - 18/11
    começando o cadastro de produtos

--- front-end/src/components/FormularioDadosDemanda/FormularioDadosDemanda.js
+++ front-end/src/components/FormularioDadosDemanda/FormularioDadosDemanda.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import FontConfig from '../../service/FontConfig'
 
 const FormularioDadosDemanda = () => {
   return (
-    <Box>
-      <Box className='outline-none border-solid border border-l-4 px-1 py-1.5 drop-shadow-sm' sx={{ borderLeftColor: 'primary.main' }} component='input' />
+    <Box sx={{height: '45rem'}}>
+      <Box className='flex'>
+        <Typography fontSize={FontConfig.default} sx={{ fontWeight: '600', cursor: 'default' }} gutterBottom>Título:</Typography>
+        <Typography fontSize={FontConfig.default} sx={{ fontWeight: '800', cursor: 'default', margin: '0 .2% .2% .2%' }} className='text-red-600' gutterBottom>*</Typography>
+      </Box>
+      <Box className='outline-none border-solid border border-l-4 px-1 py-1.5 drop-shadow-sm' sx={{ borderLeftColor: 'primary.main', width: '80%;' }} component='input' />
     </Box>
   )
 }
