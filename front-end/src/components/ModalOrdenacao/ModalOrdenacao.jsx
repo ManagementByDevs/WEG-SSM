9c9400dc6f9c5414dac7ef3e82a8de52fce1dea5
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-11-28 21:00:34 -0300

    28/11 - 21:00
    Esqueleto modal ordenação

--- front-end/src/components/ModalOrdenacao/ModalOrdenacao.jsx
+++ front-end/src/components/ModalOrdenacao/ModalOrdenacao.jsx
@@ -1,53 +1,48 @@
 import React, { useState } from "react";
 
-import { Modal, Typography, Box, Button, Checkbox, FormGroup, FormControlLabel } from '@mui/material';
+import { Modal, Typography, Box, Button, Checkbox, FormGroup, FormControlLabel, Grid } from '@mui/material';
 
 import Backdrop from '@mui/material/Backdrop';
 import Fade from '@mui/material/Fade';
 import FontConfig from '../../service/FontConfig'
+import CloseIcon from '@mui/icons-material/Close';
 
 const ModalOrdenacao = (props) => {
 
     const style = {
         position: 'absolute',
-        top: '50%',
-        left: '50%',
+        top: '46%',
+        left: '35%',
         transform: 'translate(-50%, -50%)',
-        width: 230,
-        height: 280,
+        width: 335,
+        height: 330 ,
         bgcolor: 'background.paper',
         borderRadius: '5px',
+        borderTop: '10px solid #00579D',
         boxShadow: 24,
-        p: 1,
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
         flexDirection: 'column',
+        p: 1.5,
     };
 
     const cssSelect = {
         width: '100%',
-        height: '50%',
+        height: '100%',
         display: 'flex',
-        justifyContent: 'start',
-        alignItems: 'start',
-        textAlign: 'center',
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-start',
         flexDirection: 'column',
     }
 
-    const opcoesOrenação = (tipoOrdenacao) => {
-        switch (tipoOrdenacao) {
-            case "az":
-                return "A-Z";
-            case "za":
-                return "Z-A";
-            case "ppm":
-                return "PPM";
-        }
-    };
-
-    // tipos de ordenação:
-    //  az - za - score mais alto score menor - mais novas - mais antigas -  
+    const styleDiv = {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-center',
+        marginBottom: '4%'
+    }
 
     // useState para abrir e fechar o modal
 
@@ -59,38 +54,60 @@ const ModalOrdenacao = (props) => {
 
     return (
 
-        <Modal
+        <Modal 
             open={props.open}
             onClose={handleClose}
             closeAfterTransition
-            BackdropComponent={Backdrop}
+            BackdropComponent={false}
             BackdropProps={{
                 timeout: 500,
             }}
+            disableEnforceFocus
         >
             <Fade in={props.open}>
                 <Box sx={style}>
-                    <FormGroup sx={cssSelect}>
-                        <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
-                            Título
-                        </Typography>
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="A-Z" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Z-A" />
-                    </FormGroup>
-                    <FormGroup sx={cssSelect}>
-                        <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
-                            Score
-                        </Typography>
-                        <FormControlLabel  control={<Checkbox defaultChecked />} label="Maior Score" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Menor Score" />
-                    </FormGroup>
-                    <FormGroup sx={cssSelect}>
-                        <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
-                            Data
-                        </Typography>
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Mais Nova" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Mais Velha" />
-                    </FormGroup>
+                    <CloseIcon onClick={handleClose} sx={{marginLeft: '95%'}}/>
+                    <Grid container spacing={0}>
+                        <Grid item xs={9.2}>
+                            <FormGroup sx={cssSelect}>
+                                <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
+                                    Título:
+                                </Typography>
+                                <div style={styleDiv}>
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="A-Z" />
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Z-A" />
+                                </div>
+                            </FormGroup>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container spacing={0}>
+                        <Grid item xs={20}>
+                            <FormGroup sx={cssSelect}>
+                                <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
+                                    Score:
+                                </Typography>
+                                <div style={styleDiv}>
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Maior Score" />
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Menor Score" />
+                                </div>
+                            </FormGroup>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container spacing={0}>
+                        <Grid item xs={11.4}>
+                            <FormGroup sx={cssSelect}>
+                                <Typography fontSize={FontConfig.big} sx={{ mt: 2 }}>
+                                    Data:
+                                </Typography>
+                                <div style={styleDiv}>
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Mais Nova" />
+                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Mais Velha" />
+                                </div>
+                            </FormGroup>
+                        </Grid>
+                    </Grid>
                 </Box>
             </Fade>
         </Modal>
