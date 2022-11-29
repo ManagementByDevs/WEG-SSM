e0363e07648fbd2fda237c42da3c7966a02d4a84
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-29 15:07:23 -0300

    15:07 - 29/11
    salvar para o lanche

--- front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
+++ front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
@@ -2,10 +2,25 @@ import React from "react";
 
 import { Box, Typography } from "@mui/material";
 
+import FontConfig from "../../service/FontConfig";
+
 const BeneficiosDetalheDemanda = () => {
   return (
-    <Box className="flex border rounded p-2 " sx={{ width: "80%", height: "10%" }}>
-
+    <Box className="flex border rounded p-2 pt-4" sx={{ width: "100%", height: "7rem", minWidth: '620px' }}>
+      <Box className="flex flex-col gap-2 ml-3" sx={{width: '20%'}}>
+        <Box className="flex">
+          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Tipo:</Typography>
+          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>Potencial</Typography>
+        </Box>
+        <Box className="flex flex-col">
+          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Valor Mensal:</Typography>
+          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>1000,00 BR</Typography>
+        </Box>
+      </Box>
+      <Box className="flex flex-col" sx={{width: '80%'}}>
+          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Memória de cálculo:</Typography>
+          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>Lorem Ipsum is simply dummy text of the prinpe and scrambled it to make a type specimen book.</Typography>
+      </Box>
     </Box>
   );
 };
--- front-end/src/pages/DetalhesDemanda/DetalhesDemanda.jsx
+++ front-end/src/pages/DetalhesDemanda/DetalhesDemanda.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 
 import FundoComHeader from "../../components/FundoComHeader/FundoComHeader";
 import Caminho from "../../components/Caminho/Caminho";
@@ -15,28 +15,152 @@ const DetalhesDemanda = () => {
         <Caminho />
         <Box className="flex justify-center items-center mt-3">
           <Box
-            className="flex flex-col gap-5 border rounded p-5"
-            sx={{ width: "90rem", height: "50rem" }}
+            className="flex flex-col gap-5 border rounded p-10"
+            sx={{ width: "55rem", height: "50rem" }}
           >
-            <Box className="flex justify-center" sx={{marginBottom: '20px'}}>
-                <Typography fontSize={FontConfig.title} sx={{ fontWeight: '600', cursor: 'default' }} color="primary.main">Detalhes da Demanda</Typography>
+            <Box className="flex justify-center" sx={{ marginBottom: "20px" }}>
+              <Typography
+                fontSize={FontConfig.title}
+                sx={{ fontWeight: "600", cursor: "default" }}
+                color="primary.main"
+              >
+                Detalhes da Demanda
+              </Typography>
             </Box>
             <Box>
-                <Typography fontSize={FontConfig.veryBig} fontWeight="600" color='text.primary'>Título da demanda:</Typography>
-                <Typography fontSize={FontConfig.default} className="text-justify" color='text.secondary' sx={{marginLeft: '30px'}}>Lorem Ipsum is simply dummy text of the prinpe and scrambled it to make a type specimen book.</Typography>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Título da demanda:
+              </Typography>
+              <Typography
+                fontSize={FontConfig.default}
+                className="text-justify"
+                color="text.secondary"
+                sx={{ marginLeft: "30px" }}
+              >
+                Lorem Ipsum is simply dummy text of the prinpe and scrambled it
+                to make a type specimen book.
+              </Typography>
             </Box>
             <Box>
-                <Typography fontSize={FontConfig.veryBig} fontWeight="600" color='text.primary'>Problema:</Typography>
-                <Typography fontSize={FontConfig.default} className="text-justify" color='text.secondary' sx={{marginLeft: '30px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</Typography>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Problema:
+              </Typography>
+              <Typography
+                fontSize={FontConfig.default}
+                className="text-justify"
+                color="text.secondary"
+                sx={{ marginLeft: "30px" }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries
+              </Typography>
             </Box>
             <Box>
-                <Typography fontSize={FontConfig.veryBig} fontWeight="600" color='text.primary'>Proposta:</Typography>
-                <Typography fontSize={FontConfig.default} className="text-justify" color='text.secondary' sx={{marginLeft: '30px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</Typography>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Proposta:
+              </Typography>
+              <Typography
+                fontSize={FontConfig.default}
+                className="text-justify"
+                color="text.secondary"
+                sx={{ marginLeft: "30px" }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries
+              </Typography>
             </Box>
             <Box>
-                <Typography fontSize={FontConfig.veryBig} fontWeight="600" color='text.primary'>Beneficios:</Typography>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Beneficios:
+              </Typography>
+              <Box className="ml-7 mt-2">
                 <BeneficiosDetalheDemanda />
+              </Box>
             </Box>
+            <Box>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Frequência de uso:
+              </Typography>
+              <Typography
+                fontSize={FontConfig.default}
+                className="text-justify"
+                color="text.secondary"
+                sx={{ marginLeft: "30px" }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                fontSize={FontConfig.veryBig}
+                fontWeight="600"
+                color="text.primary"
+              >
+                Anexos:
+              </Typography>
+              <Box>AQUI JÁS ANEXOS</Box>
+            </Box>
+          </Box>
+          <Box className="flex absolute" sx={{bottom: '10px', right: '10px'}}>
+          <Button
+              sx={{
+                backgroundColor: "primary.main",
+                color: "text.white",
+                fontSize: FontConfig.default,
+              }}
+              variant="contained"
+              disableElevation
+            >
+              Recusar
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "primary.main",
+                color: "text.white",
+                fontSize: FontConfig.default,
+              }}
+              variant="contained"
+              disableElevation
+            >
+              Devolver
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "primary.main",
+                color: "text.white",
+                fontSize: FontConfig.default,
+              }}
+              variant="contained"
+              disableElevation
+            >
+              Aceitar
+            </Button>
           </Box>
         </Box>
       </Box>
