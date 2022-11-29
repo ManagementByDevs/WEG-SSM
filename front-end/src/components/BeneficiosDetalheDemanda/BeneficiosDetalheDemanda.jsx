93a8e2dd01b082d5aa2a15388275759b23c44f21
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-29 13:44:49 -0300

    13:44 - 29/11
    save

---
+++ front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Box, Typography } from "@mui/material";
+
+const BeneficiosDetalheDemanda = () => {
+  return (
+    <Box className="flex border rounded p-2 " sx={{ width: "80%", height: "10%" }}>
+
+    </Box>
+  );
+};
+
+export default BeneficiosDetalheDemanda;
