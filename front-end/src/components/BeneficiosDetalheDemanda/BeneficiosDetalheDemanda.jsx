ffa646efe7ec7e3b96cd981e008cd33d24ec1770
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-29 16:56:10 -0300

    16:55 - 29/11
    Salvar para a janta,  o detalhes ainda não está pronto

--- front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
+++ front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
@@ -1,27 +1,70 @@
-import React from "react";
+import * as React from "react";
 
-import { Box, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  Paper,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 
 import FontConfig from "../../service/FontConfig";
 
 const BeneficiosDetalheDemanda = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(tipo, valorMensal, moeda, memoriaCalculo) {
+    return { tipo, valorMensal, moeda, memoriaCalculo };
+  }
+
+  const rows = [
+    createData("Real", "500,00", "BR", "Alguma coisa provavelmente grande (ou não) vem aqui, como não sei, vou escrever algo grande")
+  ];
+
   return (
-    <Box className="flex border rounded p-2 pt-4" sx={{ width: "100%", height: "7rem", minWidth: '620px' }}>
-      <Box className="flex flex-col gap-2 ml-3" sx={{width: '20%'}}>
-        <Box className="flex">
-          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Tipo:</Typography>
-          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>Potencial</Typography>
-        </Box>
-        <Box className="flex flex-col">
-          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Valor Mensal:</Typography>
-          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>1000,00 BR</Typography>
-        </Box>
-      </Box>
-      <Box className="flex flex-col" sx={{width: '80%'}}>
-          <Typography fontSize={FontConfig.medium} fontWeight='600' color="text.primary">Memória de cálculo:</Typography>
-          <Typography fontSize={FontConfig.default} color="text.secondary" sx={{ marginLeft: "10px" }}>Lorem Ipsum is simply dummy text of the prinpe and scrambled it to make a type specimen book.</Typography>
-      </Box>
-    </Box>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 750 }} aria-label="customized table">
+        <TableHead sx={{backgroundColor: '#00579D'}}>
+          <TableRow>
+            <th>Tipo</th>
+            <th>Valor Mensal</th>
+            <th>Moeda</th>
+            <th>Memória de cálculo</th>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.tipo} className="flex">
+              <td className="justify-center">{row.tipo}</td>
+              <td className="justify-center">{row.valorMensal}</td>
+              <td className="justify-center">{row.moeda}</td>
+              <td className="flex justify-center">{row.memoriaCalculo}</td>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
--- front-end/src/pages/DetalhesDemanda/DetalhesDemanda.jsx
+++ front-end/src/pages/DetalhesDemanda/DetalhesDemanda.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Divider } from "@mui/material";
 
 import FundoComHeader from "../../components/FundoComHeader/FundoComHeader";
 import Caminho from "../../components/Caminho/Caminho";
@@ -16,35 +16,18 @@ const DetalhesDemanda = () => {
         <Box className="flex justify-center items-center mt-3">
           <Box
             className="flex flex-col gap-5 border rounded p-10"
-            sx={{ width: "55rem", height: "50rem" }}
+            sx={{ width: "55rem", height: "70rem" }}
           >
-            <Box className="flex justify-center" sx={{ marginBottom: "20px" }}>
+            <Box className="flex justify-center">
               <Typography
                 fontSize={FontConfig.title}
                 sx={{ fontWeight: "600", cursor: "default" }}
                 color="primary.main"
               >
-                Detalhes da Demanda
+                Aqui tem o título da demanda
               </Typography>
             </Box>
-            <Box>
-              <Typography
-                fontSize={FontConfig.veryBig}
-                fontWeight="600"
-                color="text.primary"
-              >
-                Título da demanda:
-              </Typography>
-              <Typography
-                fontSize={FontConfig.default}
-                className="text-justify"
-                color="text.secondary"
-                sx={{ marginLeft: "30px" }}
-              >
-                Lorem Ipsum is simply dummy text of the prinpe and scrambled it
-                to make a type specimen book.
-              </Typography>
-            </Box>
+            <Divider />
             <Box>
               <Typography
                 fontSize={FontConfig.veryBig}
@@ -54,7 +37,7 @@ const DetalhesDemanda = () => {
                 Problema:
               </Typography>
               <Typography
-                fontSize={FontConfig.default}
+                fontSize={FontConfig.medium}
                 className="text-justify"
                 color="text.secondary"
                 sx={{ marginLeft: "30px" }}
@@ -75,7 +58,7 @@ const DetalhesDemanda = () => {
                 Proposta:
               </Typography>
               <Typography
-                fontSize={FontConfig.default}
+                fontSize={FontConfig.medium}
                 className="text-justify"
                 color="text.secondary"
                 sx={{ marginLeft: "30px" }}
@@ -108,7 +91,7 @@ const DetalhesDemanda = () => {
                 Frequência de uso:
               </Typography>
               <Typography
-                fontSize={FontConfig.default}
+                fontSize={FontConfig.medium}
                 className="text-justify"
                 color="text.secondary"
                 sx={{ marginLeft: "30px" }}
@@ -127,7 +110,7 @@ const DetalhesDemanda = () => {
               <Box>AQUI JÁS ANEXOS</Box>
             </Box>
           </Box>
-          <Box className="flex absolute" sx={{bottom: '10px', right: '10px'}}>
+          <Box className="flex fixed justify-around" sx={{width: '20rem',bottom: '20px', right: '20px'}}>
           <Button
               sx={{
                 backgroundColor: "primary.main",
@@ -135,7 +118,6 @@ const DetalhesDemanda = () => {
                 fontSize: FontConfig.default,
               }}
               variant="contained"
-              disableElevation
             >
               Recusar
             </Button>
