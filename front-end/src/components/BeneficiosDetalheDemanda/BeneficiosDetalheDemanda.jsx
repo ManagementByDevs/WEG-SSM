6457ffa04db3bd3cd2e159538106218d747f806f
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-30 14:35:13 -0300

    14:34 - 30/11
    arrumado o componente demanda (botao motivo estava subindo junto com o texto)

--- front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
+++ front-end/src/components/BeneficiosDetalheDemanda/BeneficiosDetalheDemanda.jsx
@@ -9,13 +9,14 @@ import {
   TableBody,
   Paper,
   Typography,
+  Box,
 } from "@mui/material";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 
 import FontConfig from "../../service/FontConfig";
 
-const BeneficiosDetalheDemanda = () => {
-
+const BeneficiosDetalheDemanda = (props) => {
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
     "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
@@ -40,80 +41,200 @@ const BeneficiosDetalheDemanda = () => {
   ];
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 750 }} aria-label="customized table">
-        <TableHead sx={{ backgroundColor: "primary.main" }}>
-          <TableRow>
-            <th align="center" className="p-4 w-0">
-              <Typography
-                fontSize={FontConfig.big}
-                fontWeight="800"
-                color="text.white"
-                sx={{width: '40%'}}
-              >
-                Tipo
-              </Typography>
-            </th>
-            <th align="center" className="p-4 w-0">
-              <Typography
-                fontSize={FontConfig.big}
-                fontWeight="800"
-                color="text.white"
-                sx={{width: '40%'}}
-              >
-                Valor Mensal
-              </Typography>
-            </th>
-            <th align="center" className="p-4 w-0">
-              <Typography
-                fontSize={FontConfig.big}
-                fontWeight="800"
-                color="text.white"
-                sx={{width: '40%'}}
-              >
-                Moeda
-              </Typography>
-            </th>
-            <th align="center" className="p-4 w-0">
-              <Typography
-                fontSize={FontConfig.big}
-                fontWeight="800"
-                color="text.white"
-                sx={{width: '40%'}}
-              >
-                Memória de Cálculo
-              </Typography>
-            </th>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.tipo} className="flex">
-              <td align="center">
-                <Typography fontSize={FontConfig.medium} color="text.primary">
-                  {row.tipo}
-                </Typography>
-              </td>
-              <td align="center">
-                <Typography fontSize={FontConfig.medium} color="text.primary">
-                  {row.valorMensal}
-                </Typography>
-              </td>
-              <td align="center">
-                <Typography fontSize={FontConfig.medium} color="text.primary">
-                  {row.moeda}
-                </Typography>
-              </td>
-              <td align="center" className="p-3 pl-5 pr-5 flex justify-center">
-                <Typography className="text-justify" fontSize={FontConfig.medium} color="text.primary" sx={{width: '100%'}}>
-                  {row.memoriaCalculo}
-                </Typography>
-              </td>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box className="flex items-center">
+      {props.editavel ? (
+        <>
+          <DeleteOutlineOutlinedIcon
+            fontSize="large"
+            className="delay-120 hover:scale-110 duration-300 mr-2"
+            sx={{ color: "icon.main", cursor: "pointer" }}
+          />
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 750 }} aria-label="customized table">
+              <TableHead sx={{ backgroundColor: "primary.main" }}>
+                <TableRow>
+                  <th align="center" className="p-4 w-0">
+                    <Typography
+                      fontSize={FontConfig.big}
+                      fontWeight="800"
+                      color="text.white"
+                      sx={{ width: "40%" }}
+                    >
+                      Tipo
+                    </Typography>
+                  </th>
+                  <th align="center" className="p-4 w-0">
+                    <Typography
+                      fontSize={FontConfig.big}
+                      fontWeight="800"
+                      color="text.white"
+                      sx={{ width: "40%" }}
+                    >
+                      Valor Mensal
+                    </Typography>
+                  </th>
+                  <th align="center" className="p-4 w-0">
+                    <Typography
+                      fontSize={FontConfig.big}
+                      fontWeight="800"
+                      color="text.white"
+                      sx={{ width: "40%" }}
+                    >
+                      Moeda
+                    </Typography>
+                  </th>
+                  <th align="center" className="p-4 w-0">
+                    <Typography
+                      fontSize={FontConfig.big}
+                      fontWeight="800"
+                      color="text.white"
+                      sx={{ width: "40%" }}
+                    >
+                      Memória de Cálculo
+                    </Typography>
+                  </th>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <StyledTableRow key={row.tipo} className="flex">
+                    <td align="center">
+                      <Typography
+                        fontSize={FontConfig.medium}
+                        color="text.primary"
+                      >
+                        {row.tipo}
+                      </Typography>
+                    </td>
+                    <td align="center">
+                      <Typography
+                        fontSize={FontConfig.medium}
+                        color="text.primary"
+                      >
+                        {row.valorMensal}
+                      </Typography>
+                    </td>
+                    <td align="center">
+                      <Typography
+                        fontSize={FontConfig.medium}
+                        color="text.primary"
+                      >
+                        {row.moeda}
+                      </Typography>
+                    </td>
+                    <td
+                      align="center"
+                      className="p-3 pl-5 pr-5 flex justify-center"
+                    >
+                      <Typography
+                        className="text-justify"
+                        fontSize={FontConfig.medium}
+                        color="text.primary"
+                        sx={{ width: "100%" }}
+                      >
+                        {row.memoriaCalculo}
+                      </Typography>
+                    </td>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 750 }} aria-label="customized table">
+            <TableHead sx={{ backgroundColor: "primary.main" }}>
+              <TableRow>
+                <th align="center" className="p-4 w-0">
+                  <Typography
+                    fontSize={FontConfig.big}
+                    fontWeight="800"
+                    color="text.white"
+                    sx={{ width: "40%" }}
+                  >
+                    Tipo
+                  </Typography>
+                </th>
+                <th align="center" className="p-4 w-0">
+                  <Typography
+                    fontSize={FontConfig.big}
+                    fontWeight="800"
+                    color="text.white"
+                    sx={{ width: "40%" }}
+                  >
+                    Valor Mensal
+                  </Typography>
+                </th>
+                <th align="center" className="p-4 w-0">
+                  <Typography
+                    fontSize={FontConfig.big}
+                    fontWeight="800"
+                    color="text.white"
+                    sx={{ width: "40%" }}
+                  >
+                    Moeda
+                  </Typography>
+                </th>
+                <th align="center" className="p-4 w-0">
+                  <Typography
+                    fontSize={FontConfig.big}
+                    fontWeight="800"
+                    color="text.white"
+                    sx={{ width: "40%" }}
+                  >
+                    Memória de Cálculo
+                  </Typography>
+                </th>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.tipo} className="flex">
+                  <td align="center">
+                    <Typography
+                      fontSize={FontConfig.medium}
+                      color="text.primary"
+                    >
+                      {row.tipo}
+                    </Typography>
+                  </td>
+                  <td align="center">
+                    <Typography
+                      fontSize={FontConfig.medium}
+                      color="text.primary"
+                    >
+                      {row.valorMensal}
+                    </Typography>
+                  </td>
+                  <td align="center">
+                    <Typography
+                      fontSize={FontConfig.medium}
+                      color="text.primary"
+                    >
+                      {row.moeda}
+                    </Typography>
+                  </td>
+                  <td
+                    align="center"
+                    className="p-3 pl-5 pr-5 flex justify-center"
+                  >
+                    <Typography
+                      className="text-justify"
+                      fontSize={FontConfig.medium}
+                      color="text.primary"
+                      sx={{ width: "100%" }}
+                    >
+                      {row.memoriaCalculo}
+                    </Typography>
+                  </td>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </Box>
   );
 };
 
--- front-end/src/components/Demanda/Demanda.jsx
+++ front-end/src/components/Demanda/Demanda.jsx
@@ -1,79 +1,130 @@
-import { Box, Typography, Button } from '@mui/material'
-import React, {useState} from 'react'
-import FontConfig from '../../service/FontConfig'
+import { Box, Typography, Button } from "@mui/material";
+import React, { useState } from "react";
+import FontConfig from "../../service/FontConfig";
 
-import ModalMotivoRecusa from '../ModalMotivoRecusa/ModalMotivoRecusa'
+import ModalMotivoRecusa from "../ModalMotivoRecusa/ModalMotivoRecusa";
 
 const Demanda = (props) => {
-    let autor = props.demanda.dono;
-    let status = props.demanda.status;
-    let tela = props.demanda.tela;
-    let userLogado = "Thiago";
-    let corStatus = getStatusColor();
-    let tamanhoHeight = getTamanhoHeight();
+  let autor = props.demanda.dono;
+  let status = props.demanda.status;
+  let tela = props.demanda.tela;
+  let userLogado = "Thiago";
+  let corStatus = getStatusColor();
+  let tamanhoHeight = getTamanhoHeight();
 
-    function getStatusColor() {
-        switch (status) {
-            case 'Aguardando edição':
-                return '#FFD600';
-            case 'Aguardando revisão':
-                return '#C4C4C4';
-            case 'Aprovada':
-                return '#11B703';
-            case 'Reprovada':
-                return '#DA0303';
-            default:
-                return '#DA0303';
-        }
+  function getStatusColor() {
+    switch (status) {
+      case "Aguardando edição":
+        return "#FFD600";
+      case "Aguardando revisão":
+        return "#C4C4C4";
+      case "Aprovada":
+        return "#11B703";
+      case "Reprovada":
+        return "#DA0303";
+      default:
+        return "#DA0303";
     }
+  }
 
-    function getTamanhoHeight() {
-        if (tela !== "minhasDemandas") {
-            return '10rem';
-        } else {
-            return '12rem';
-        }
+  function getTamanhoHeight() {
+    if (tela !== "minhasDemandas") {
+      return "10rem";
+    } else {
+      return "12rem";
     }
+  }
 
-    // useState para abrir o modal de motivo recusa
+  // useState para abrir o modal de motivo recusa
 
-    const [abrirModal, setOpenModal] = useState(false);
+  const [abrirModal, setOpenModal] = useState(false);
 
-    const abrirModalMotivoRecusa = () =>{
-        setOpenModal(true);
-    }
+  const abrirModalMotivoRecusa = () => {
+    setOpenModal(true);
+  };
 
-    return (
-        <Box onClick={props.onClick} sx={{ backgroundColor: 'background.default', padding: '1rem', minWidth: '550px', maxWidth: '100%', minHeight: tamanhoHeight, maxHeight: '12rem', border: '0.2px solid black', borderTop: '7px solid #00579D', borderRadius: '5px', cursor: 'pointer' }} className={`items-center h-30 text-justify drop-shadow-lg`}>
-            <Box className={`flex justify-between`} sx={{ marginBottom: '1%' }}>
-                <Typography fontSize={FontConfig.veryBig} sx={{ fontWeight: '600', cursor: 'default' }} color="text.primary">Título da proposta</Typography>
-                {
-                    autor === "Thiago" && tela === "minhasDemandas"?
-                    <Box className={`items-center text-justify flex`}>
-                        <Typography fontSize={FontConfig.default} sx={{ fontWeight: '600', cursor: 'default' }}>{status}</Typography>
-                        <Box sx={{ backgroundColor: corStatus, width: '12px', height: '12px', borderRadius: '10px', marginLeft: '10px' }} className={`items-center h-30 text-justify`} />
-                    </Box>
-                    : null
-                }
-                
-            </Box>
-            
-            <Typography gutterBottom fontSize={FontConfig.default} color="text.secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</Typography>
-            <Box className={`flex justify-end`} sx={{ marginTop: '.5%' }}>
-                {
-                    status === 'Aguardando revisão' && autor !== userLogado ?
-                        <Typography fontSize={FontConfig.default} sx={{ fontWeight: '600', cursor: 'default' }} color="text.primary">{autor}</Typography>
-                        : status === 'Reprovada' && autor === userLogado?
-                            <Button onClick={abrirModalMotivoRecusa} variant="contained">Motivo</Button>
-                        :null
-                }
-                {/* Abrindo o modal de motivo recusa */}
-                {abrirModal && <ModalMotivoRecusa open={abrirModal} setOpen={setOpenModal} motivoRecusa={"Aqui vai o texto de motivo da recusa"} />}
-            </Box>
+  return (
+    <Box
+      onClick={props.onClick}
+      sx={{
+        backgroundColor: "background.default",
+        padding: "1rem",
+        minWidth: "550px",
+        maxWidth: "100%",
+        minHeight: tamanhoHeight,
+        maxHeight: "12rem",
+        border: "0.2px solid black",
+        borderTop: "7px solid #00579D",
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+      className={`items-center h-30 text-justify drop-shadow-lg`}
+    >
+      <Box className="flex flex-col justify-between" sx={{height: '100%'}}>
+        <Box>
+          <Box className={`flex justify-between`} sx={{ marginBottom: "1%" }}>
+            <Typography
+              fontSize={FontConfig.veryBig}
+              sx={{ fontWeight: "600", cursor: "default" }}
+              color="text.primary"
+            >
+              Título da proposta
+            </Typography>
+            {autor === "Thiago" && tela === "minhasDemandas" ? (
+              <Box className={`items-center text-justify flex`}>
+                <Typography
+                  fontSize={FontConfig.default}
+                  sx={{ fontWeight: "600", cursor: "default" }}
+                >
+                  {status}
+                </Typography>
+                <Box
+                  sx={{
+                    backgroundColor: corStatus,
+                    width: "12px",
+                    height: "12px",
+                    borderRadius: "10px",
+                    marginLeft: "10px",
+                  }}
+                  className={`items-center h-30 text-justify`}
+                />
+              </Box>
+            ) : null}
+          </Box>
+          <Typography
+            gutterBottom
+            fontSize={FontConfig.default}
+            color="text.secondary"
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </Typography>
         </Box>
-    )
-}
+        <Box className={`flex justify-end items-end`} sx={{ marginTop: ".5%" }}>
+          {status === "Aguardando revisão" && autor !== userLogado ? (
+            <Typography
+              fontSize={FontConfig.default}
+              sx={{ fontWeight: "600", cursor: "default" }}
+              color="text.primary"
+            >
+              {autor}
+            </Typography>
+          ) : status === "Reprovada" && autor === userLogado ? (
+            <Button onClick={abrirModalMotivoRecusa} variant="contained">
+              Motivo
+            </Button>
+          ) : null}
+          {/* Abrindo o modal de motivo recusa */}
+          {abrirModal && (
+            <ModalMotivoRecusa
+              open={abrirModal}
+              setOpen={setOpenModal}
+              motivoRecusa={"Aqui vai o texto de motivo da recusa"}
+            />
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-
-
-export default Demanda
+export default Demanda;
