1ba5aa89a545574f855ba7e167506df956d8366d
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-12-12 18:35:54 -0300

    18:35 - 12/12
    arrumando merdas

--- front-end/src/components/FormularioPropostaProposta/FormularioPropostaProposta.jsx
+++ front-end/src/components/FormularioPropostaProposta/FormularioPropostaProposta.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Box } from "@mui/material";
 
 import DetalhesDemanda from "../DetalhesDemanda/DetalhesDemanda";
 
 const FormularioPropostaProposta = () => {
+  const [dados, setDados] = useState({
+    titulo: "Sistema de Gestão de Demandas",
+    problema:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    proposta:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  book. It has survived not only five centuries is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since  the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    frequencia: "Lorem Ipsum is simply dummy text of the printing and",
+    beneficios: [
+      {
+        tipo: "Real",
+        valorMensal: "300,00",
+        moeda: "BR",
+        memoriaCalculo:
+          "aqui vai a memória de cálculo, onde conterá as informações necessárias dele",
+        visible: true,
+      },
+    ],
+  });
+
   return (
     <Box sx={{ height: "42.5rem" }}>
-      <DetalhesDemanda />
+      <DetalhesDemanda dados={dados} setDados={setDados} />
     </Box>
   );
 };
