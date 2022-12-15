2fb6357752fe435cfcb8e18801defe6212eb5cfb
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-12-14 21:49:44 -0300

    14/12 - 21:48
    começado routing
    Lógica: preciso de um user compartilhado em todas as telas, para isso estou criando um context que irá prover o user (não sei se o setUser irá funcionar, lembrar de testar)
    Lembrar também de ver se a variável irá atualizar em todas as páginas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useMemo } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
@@ -12,32 +17,68 @@ import HomeGerencia from "./pages/HomeGerencia/HomeGerencia";
 import Escopos from "./pages/Escopos/Escopos";
 import DetalhesAta from "./pages/DetalhesAta/DetalhesAta";
 import DetalhesPauta from "./pages/DetalhesPauta/DetalhesPauta";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 import ToggleColorMode from "./service/TemaProvedor";
 import CriarProposta from "./pages/CriarProposta/CriarProposta";
 import EditarEscopo from "./pages/EditarEscopo/EditarEscopo";
 
+import UserContext from "./service/UserContext";
+
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const userValueContext = useMemo(
+    () => ({
+      user,
+      setUser: () => {
+        setUser();
+      },
+    }),
+    [user]
+  );
+
   return (
     <ToggleColorMode>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/home-gerencia" element={<HomeGerencia />} />
-          <Route path="/criar-demanda" element={<CriarDemanda />} />
-          <Route path="/criar-proposta" element={<CriarProposta />} />
-          <Route path="/notificacao" element={<Notificacao />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/detalhes-demanda" element={<DetalhesDemandaPagina />} />
-          <Route path="/editar-escopo" element={<EditarEscopo />} />
-          <Route path="escopos" element={<Escopos />} />
-          <Route path="detalhes-ata" element={<DetalhesAta />} />
-          <Route path="detalhes-pauta" element={<DetalhesPauta />} />
-        </Routes>
-      </Router>
+      <UserContext.Provider value={userValueContext}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/home-gerencia" element={<HomeGerencia />} />
+            <Route path="/criar-demanda" element={<CriarDemanda />} />
+            <Route path="/criar-proposta" element={<CriarProposta />} />
+            <Route path="/notificacao" element={<Notificacao />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route
+              path="/detalhes-demanda"
+              element={<DetalhesDemandaPagina />}
+            />
+            <Route path="/editar-escopo" element={<EditarEscopo />} />
+            <Route path="escopos" element={<Escopos />} />
+            <Route path="detalhes-ata" element={<DetalhesAta />} />
+            <Route path="detalhes-pauta" element={<DetalhesPauta />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </ToggleColorMode>
   );
+};
+
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
 
 export default App;
