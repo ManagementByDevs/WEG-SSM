685c24cc62b924d218448d52c29cb44408638c90
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2023-03-27 18:24:59 -0300

    27/03 - 18:24
    Começando exportação para excel das atas

--- back-end-maven/src/main/java/net/weg/wegssm/model/service/ExcelGeneratorService.java
+++ back-end-maven/src/main/java/net/weg/wegssm/model/service/ExcelGeneratorService.java
@@ -438,7 +438,6 @@ public class ExcelGeneratorService {
     }
 
     public void exportPautasToExcel(HttpServletResponse response, List<Long> listaPautas) throws IOException {
-        PautaUtil pautaUtil = new PautaUtil();
         ArrayList<Optional<Pauta>> listPautas = new ArrayList<>();
 
         // Convertendo para pauta e adicioando na lista das pautas
@@ -616,7 +615,181 @@ public class ExcelGeneratorService {
         outputStream.close();
     }
 
-    public void exportAtasToExcel(HttpServletResponse response, List<String> listaAtas) throws IOException {
+    public void exportAtasToExcel(HttpServletResponse response, List<Long> listaAtas) throws IOException {
+//        ArrayList<Optional<Ata>> listAtas = new ArrayList<>();
+//
+//        // Convertendo para pauta e adicioando na lista das pautas
+//        for (int i = 0; i < listaAtas.size(); i++) {
+//            Long idAta = listaAtas.get(i);
+//            Optional<Ata> ata = ataService.findById(idAta);
+//            listAtas.add(ata);
+//        }
+//
+//        // Cria um workbook do Excel e uma planilha
+//        XSSFWorkbook workbook = new XSSFWorkbook();
+//        XSSFSheet sheet = workbook.createSheet("Atas");
+//
+//        // Pegando a data atual para colocar no título do documento
+//        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy:hh:mm:ss");
+//        String currentDateTime = dateFormatter.format(new Date());
+//
+//        // Informações para o documento
+//        response.setHeader("Content-Disposition", "attachment; filename=atas - " + currentDateTime + ".xlsx");
+//        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+//
+//        // Crinado estilos e fontes para o arquivo
+//        CellStyle style = workbook.createCellStyle();
+//
+//        Font font = workbook.createFont();
+//        font.setBold(true);
+//
+//        style.setFont(font);
+//        style.setAlignment(HorizontalAlignment.CENTER);
+//
+//        CellStyle alignLeft = workbook.createCellStyle();
+//        alignLeft.setIndention((short) 2);
+//
+//        // Cria uma linha de cabeçalho para a planilha
+//        XSSFRow headerRow = sheet.createRow(0);
+//
+//        // Criando os títulos das colunas
+//        headerRow.createCell(0).setCellValue("ID");
+//        headerRow.createCell(1).setCellValue("Ata");
+//        headerRow.createCell(2).setCellValue("Propostas");
+//        headerRow.createCell(3).setCellValue("PPM");
+//        headerRow.createCell(4).setCellValue("Data");
+//        headerRow.createCell(5).setCellValue("Solicitante");
+//        headerRow.createCell(6).setCellValue("Gerente");
+//        headerRow.createCell(7).setCellValue("Problema");
+//        headerRow.createCell(8).setCellValue("Proposta");
+//        headerRow.createCell(9).setCellValue("Escopo");
+//        headerRow.createCell(10).setCellValue("Custos");
+//        headerRow.createCell(11).setCellValue("Benefícios");
+//        headerRow.createCell(12).setCellValue("Frequência de Uso");
+//        headerRow.createCell(13).setCellValue("Link Jira");
+//        headerRow.createCell(14).setCellValue("Período Execução");
+//        headerRow.createCell(15).setCellValue("Payback");
+//        headerRow.createCell(16).setCellValue("Tamanho");
+//        headerRow.createCell(17).setCellValue("Seção TI");
+//        headerRow.createCell(18).setCellValue("BU Solicitante");
+//        headerRow.createCell(19).setCellValue("BUs Beneficiadas");
+//        headerRow.createCell(20).setCellValue("Fórum");
+//        headerRow.createCell(21).setCellValue("Responsáveis Negócio");
+//        headerRow.createCell(22).setCellValue("Anexos");
+//
+//        // Setando o estilo para as colunas
+//        int startColIndex = 0;
+//        int endColIndex = 22;
+//        int rowColIndex = 0;
+//
+//        for (int colIndex = startColIndex; colIndex <= endColIndex; colIndex++) {
+//            Cell cell = sheet.getRow(rowColIndex).getCell(colIndex);
+//            cell.setCellStyle(style);
+//        }
+//
+//        int rowNum = 1;
+//        int contadorPauta = 1;
+//        for (Optional<Pauta> pautaOp : listPautas) {
+//
+//            XSSFRow row = sheet.createRow(rowNum++);
+//
+//            int rowIndex = rowNum;
+//
+//            if(pautaOp.isPresent()) {
+//                Pauta pauta = pautaOp.get();
+//
+//                row.createCell(0).setCellValue(contadorPauta);
+//                row.createCell(1).setCellValue("Número Sequencial: " + pauta.getNumeroSequencial() + "    Comissão: " + pauta.getComissao().getSiglaForum() + " - " + pauta.getComissao().getNomeForum() + "    Reunião do Fórum: " + pauta.getDataReuniao() + "    Analista Responsável: " + pauta.getAnalistaResponsavel().getNome());
+//
+//                boolean primeiraProposta = true;
+//
+//                for (Proposta proposta : pauta.getPropostas()) {
+//                    if (!primeiraProposta) {
+//                        Row rowVazia = sheet.createRow(rowIndex++);
+//                    } else {
+//                        primeiraProposta = false;
+//                    }
+//
+//                    row.createCell(2).setCellValue(proposta.getTitulo());
+//                    row.createCell(3).setCellValue(proposta.getCodigoPPM());
+//
+//                    row.createCell(4).setCellValue(proposta.getData());
+//                    row.createCell(5).setCellValue(proposta.getSolicitante().getNome() + " - " + proposta.getSolicitante().getDepartamento().getNome());
+//                    row.createCell(6).setCellValue(proposta.getGerente().getNome() + " - " + proposta.getGerente().getDepartamento().getNome());
+//                    row.createCell(7).setCellValue(proposta.getProblema());
+//                    row.createCell(8).setCellValue(proposta.getProposta());
+//
+//                    for (TabelaCusto tbCusto : proposta.getTabelaCustos()) {
+//                        for (Custo custo : tbCusto.getCustos()) {
+//                            Row rowCusto = sheet.createRow(rowIndex);
+//                            rowCusto.createCell(10).setCellValue("Tipo Despesa: " + custo.getTipoDespesa() + "  " + "Perfil Despesa: " + custo.getPerfilDespesa() + "  " + "Período de Execução (meses): " + custo.getPeriodoExecucao() + "  " + "Horas: " + custo.getHoras() + "  " + "Valor Hora: " + custo.getValorHora() + "  " + "Total: 100");
+//                        }
+//                    }
+//
+//                    for (Beneficio beneficio : proposta.getBeneficios()) {
+//                        Row roww = sheet.createRow(rowIndex);
+//                        roww.createCell(11).setCellValue("Tipo: " + String.valueOf(beneficio.getTipoBeneficio()) + "  " + " Valor Mensal: " + beneficio.getValor_mensal() + "  " + " Moeda: " + beneficio.getMoeda() + "  " + " Memória de Cálculo: " + beneficio.getMemoriaCalculo());
+//                        rowIndex++;
+//                    }
+//
+//                    row.createCell(12).setCellValue(proposta.getFrequencia());
+//                    row.createCell(13).setCellValue(proposta.getLinkJira());
+//                    row.createCell(14).setCellValue(proposta.getInicioExecucao() + " à " + proposta.getFimExecucao());
+//                    row.createCell(15).setCellValue(proposta.getPaybackValor() + "  " + proposta.getPaybackTipo());
+//                    row.createCell(16).setCellValue(proposta.getTamanho());
+//                    row.createCell(17).setCellValue(proposta.getSecaoTI().getSiglaSecao() + " - " + proposta.getSecaoTI().getNomeSecao());
+//                    row.createCell(18).setCellValue(proposta.getBuSolicitante().getSiglaBu() + "  " + proposta.getBuSolicitante().getNomeBu());
+//
+//                    for (Bu bu : proposta.getBusBeneficiadas()) {
+//                        row.createCell(19).setCellValue(" - " + bu.getSiglaBu() + " - " + bu.getNomeBu() + "  ");
+//                        row.getCell(19).setCellStyle(alignLeft);
+//                    }
+//
+//                    row.createCell(20).setCellValue(proposta.getForum().getSiglaForum() + " - " + proposta.getForum().getNomeForum());
+//
+//                    for (ResponsavelNegocio rn : proposta.getResponsavelNegocio()) {
+//                        row.createCell(21).setCellValue(" - " + rn.getNome() + " - " + rn.getArea() + "  ");
+//                        row.getCell(21).setCellStyle(alignLeft);
+//                    }
+//
+//                    for (Anexo anexo : proposta.getAnexo()) {
+//                        row.createCell(22).setCellValue(" - " + anexo.getNome() + ". " + anexo.getTipo());
+//                        row.getCell(22).setCellStyle(alignLeft);
+//                    }
+//
+//                    // Aplicando o estilo nas linhas
+////                    int startRowIndex = 0;
+////                    int endRowIndex = 21;
+////                    int colIndex = 0;
+////
+////                    for (int rowwIndex = startRowIndex; rowwIndex <= endRowIndex; rowwIndex++) {
+////                        Cell cell = sheet.getRow(rowwIndex).getCell(colIndex);
+////                        if(cell != null) {
+////                            cell.setCellStyle(alignLeft);
+////                        }
+////                    }
+//
+//                    // Setando os estilos para as colunas
+//                    row.getCell(0).setCellStyle(style);
+//
+//                    // Auto ajustando o tamanho das colunas de acordo com as informações
+//                    Row headerRowAutoSize = sheet.getRow(0);
+//                    int lastCellNum = headerRowAutoSize.getLastCellNum();
+//
+//                    for (int i = 0; i < lastCellNum; i++) {
+//                        sheet.autoSizeColumn(i);
+//                    }
+//                }
+//            }
+//
+//            rowNum = rowIndex;
+//            contadorPauta++;
+//        }
 
+//        // Escreve o workbook no response
+//        OutputStream outputStream = response.getOutputStream();
+//        workbook.write(outputStream);
+//        workbook.close();
+//        outputStream.close();
     }
 }
