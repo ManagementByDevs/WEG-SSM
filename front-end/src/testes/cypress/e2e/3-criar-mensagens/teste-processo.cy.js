49e3fea473824b92e8b99adcb9a64c7196b5b6ce
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2023-05-09 14:24:38 -0300

    09/05 - 14:24
    endpoint criar mensagem feito

--- front-end/src/testes/cypress/e2e/3-criar-mensagens/teste-processo.cy.js
+++ front-end/src/testes/cypress/e2e/3-criar-mensagens/teste-processo.cy.js
@@ -15,32 +15,44 @@ describe("Teste de processo do endPoint criar mensagen", () => {
     cy.request("GET", "http://localhost:8443/weg_ssm/mensagem").as(
       "BuscarMensagens"
     );
-    cy.get("@BuscarMensagens").then((response) => {
-      qtdMensagens = response.body.length;
-      expect(response.status).to.eq(200);
+
+    cy.get("@BuscarMensagens").then((getResponse) => {
+      qtdMensagens = getResponse.body.length;
+      expect(getResponse.status).to.eq(200);
     });
   });
 
-  it("Criar mensagem", () => {
+  it("Criar Mensagem", () => {
     let form = new FormData();
-
     form.append("mensagem", JSON.stringify(mensagem));
 
-    cy.request({
-      method: "POST",
-      url: "http://localhost:8443/weg_ssm/mensagem",
-      body: form,
-    }).then((response) => {
-      expect(response.status).to.equal(200);
+    cy.request("POST", "http://localhost:8443/weg_ssm/login/auth", {
+      email: "kenzo@gmail",
+      senha: "123",
+    }).then((authResponse) => {
+      expect(authResponse.status).to.eq(200);
+      cy.request({
+        method: "POST",
+        url: "http://localhost:8443/weg_ssm/mensagem",
+        body: form,
+      }).then((postResponse) => {
+        expect(postResponse.status).to.equal(201);
+      });
     });
   });
 
-  it("Verificar quantidade de menesagens", () => {
-    cy.request("GET", "http://localhost:8443/weg_ssm/mensagem").as(
-      "BuscarMensagens"
-    );
-    cy.get("@BuscarMensagens").then((response) => {
-      expect(response.body).to.eq(qtdMensagens + 1);
+  it("Verificar quantidade de mensagens", () => {
+    cy.request("POST", "http://localhost:8443/weg_ssm/login/auth", {
+      email: "kenzo@gmail",
+      senha: "123",
+    }).then((authResponse) => {
+      expect(authResponse.status).to.eq(200);
+
+      cy.request("GET", "http://localhost:8443/weg_ssm/mensagem")
+        .as("BuscarMensagens")
+        .then((response) => {
+          expect(response.body.length).to.eq(qtdMensagens + 1);
+        });
     });
   });
 });
